8298abfa65bffd822fdc1b38745a8a378f1e1652
caleb <caleb@beyondgames.co>
2015-12-04 15:01:19 -0600

    string xml

--- ProtoModels/prebuild.fsx
+++ ProtoModels/prebuild.fsx
@@ -10,8 +10,36 @@ open Fake.ConfigurationHelper
 open Fake.XMLHelper
 open System.Xml
 let config = readConfig "ProtoModels.csproj"
+let xmlString = @"
+<Project DefaultTargets=""Build"" ToolsVersion=""4.0"" xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">
+  <PropertyGroup>
+    <Configuration Condition="" '$(Configuration)' == '' "">Debug</Configuration>
+    <Platform Condition="" '$(Platform)' == '' "">AnyCPU</Platform>
+    <ProjectGuid>{672A1EB8-4BC6-410A-BE7E-6251A0DB7AD6}</ProjectGuid>
+    <OutputType>Library</OutputType>
+    <RootNamespace>ProtoModels</RootNamespace>
+    <AssemblyName>ProtoModels</AssemblyName>
+    <TargetFrameworkVersion>v4.5</TargetFrameworkVersion>
+  </PropertyGroup>
+  <ItemGroup>
+    <Reference Include=""System"" />
+  </ItemGroup>
+  <ItemGroup>
+    <Compile Include=""Properties\AssemblyInfo.cs"" />
+    <Compile Include=""models\Test.cs"" />
+    <Compile Include=""models\Request.cs"" />
+  </ItemGroup>
+  <Import Project=""$(MSBuildBinPath)\Microsoft.CSharp.targets"" />
+  <ItemGroup>
+    <Folder Include=""schemas\"" />
+    <Folder Include=""models\"" />
+  </ItemGroup>
+</Project>"
+let xml = new XmlDocument();
+xml.LoadXml(xmlString); // suppose that myXmlString contains "<Names>...</Names>"
 
-let nsmgr = XmlNamespaceManager(config.NameTable)
-
-printfn "%A" <| (config.DocumentElement.SelectNodes("//project")).Count
+let xnList = 
+    xml.SelectNodes("/Project/ItemGroup");
+printfn "%A" xnList.Count
+printfn "%A" <| (config.DocumentElement.SelectNodes("//ItemGroup")).Count
 
